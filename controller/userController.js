b2eb942e10ed3b4373ee79007c8fc577c47746b7
shanishaju <shanishaju84210@gmail.com>
2024-07-17 15:24:38 +0530

    day2

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,9 @@
 //register
 
 exports.registerController=(req,res)=>{
-    
-       
+
+       const {username , email , password}= req.body
+       console.log(username, email, password);
+
+       res.status(200).json('register requeest recieved')
 }
