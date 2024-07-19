05f777b68ad11885b91619b53c3fe8a4c90fd86d
shanishaju <shanishaju84210@gmail.com>
2024-07-20 01:37:22 +0530

    db created

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,34 @@
 //register
 
-exports.registerController=(req,res)=>{
+const users = require("../modal/userModal");
+
+exports.registerController= async(req,res)=>{
 
        const {username , email , password}= req.body
        console.log(username, email, password);
 
-       res.status(200).json('register requeest recieved')
+       // res.status(200).json('register requeest recieved')
+       try {
+          const existingUser = await users.findOne({email})
+           if(existingUser){
+              res.status(406).json('Account Already Exist')
+           }
+           else{
+              const newUser = new users({
+                     username,
+                     email,
+                     password,
+                     github:"",
+                     linkedin:"",
+                     profile:""
+              })
+              //save() method used to store in mongodb
+              await newUser.save()
+             res.status(200).json(newUser)
+
+           }
+              
+       } catch (error) {
+              res.status(401).json(`registration failed due to ${error} `)     
+       }
 }
---
+++ modal/userModal.js
@@ -0,0 +1,34 @@
+//import mongoose
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    github:{
+        type:String,
+
+    },
+    linkedin:{
+        type:String
+    },
+    profile:{
+        type:String
+    }
+})
+
+
+const users= mongoose.model("users",userSchema)
+
+//export 
+module.exports=users
